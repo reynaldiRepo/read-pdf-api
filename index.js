fedf1bf8ca4d91621b17452abe02cce103b9542f
Reynaldi <38524172+reynaldiRepo@users.noreply.github.com>
2020-07-31 21:01:32 +0700

    finishiing function read

--- index.js
+++ index.js
@@ -5,17 +5,37 @@ const app = express();
 const fs = require("fs");
 const crawler = require('crawler-request')
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+
+
 // for try pdf url
 var url = "https://www.soundczech.cz/temp/lorem-ipsum.pdf"
 
 
 app.get("/", (req, res) => {
-    // res.send("welcome to reynaldLancer "+ new Date())
     crawler(url).then(function(response){
         console.log(response)
         res.json(response)
     })
 })
+
+app.post("/read", (req, res)=>{
+    console.log(req.body)
+    pdfurl = req.body.URL;
+    crawler(pdfurl).then(function(response){
+        console.log(response)
+        res.json(response)
+    })
+    
+})
+
+
 
 
 
